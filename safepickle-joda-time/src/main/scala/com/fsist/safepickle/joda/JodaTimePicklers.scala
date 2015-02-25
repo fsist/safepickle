85d90156cd170b17f8b6ea4d2d3e268a0b560c5f
Daniel Armak <danarmak@gmail.com>
2015-02-25 22:13:04 +0200

    Remove code that's no longer needed

--- safepickle-joda-time/src/main/scala/com/fsist/safepickle/joda/JodaTimePicklers.scala
+++ safepickle-joda-time/src/main/scala/com/fsist/safepickle/joda/JodaTimePicklers.scala
@@ -1,33 +1,27 @@
 package com.fsist.safepickle.joda
 
 import com.fsist.safepickle._
-import org.joda.time.{DateTime, Interval, Duration, Instant}
+import org.joda.time._
 
 /** Picklers for joda-time types which, by default, pickle them as Long values counting milliseconds. */
 object JodaTimePicklers {
   implicit object InstantPickler extends Pickler[Instant] with ConvertPickler[Instant, Long] {
-    override implicit def otherPickler: Pickler[Long] = PrimitivePicklers.LongPickler
     override def convertFrom(other: Long): Instant = new Instant(other)
     override def convertTo(t: Instant): Long = t.getMillis
   }
 
   implicit object DateTimePickler extends Pickler[DateTime] with ConvertPickler[DateTime, Long] {
-    override implicit def otherPickler: Pickler[Long] = PrimitivePicklers.LongPickler
     override def convertFrom(other: Long): DateTime = new DateTime(other)
     override def convertTo(t: DateTime): Long = t.getMillis
   }
 
   implicit object DurationPickler extends Pickler[Duration] with ConvertPickler[Duration, Long] {
-    override implicit def otherPickler: Pickler[Long] = PrimitivePicklers.LongPickler
     override def convertFrom(other: Long): Duration = new Duration(other)
     override def convertTo(t: Duration): Long = t.getMillis
   }
 
   /** Pickles an interval as an array of start and end dates. */
   implicit object IntervalPickler extends Pickler[Interval] with ConvertPickler[Interval, (Long, Long)] with TuplePicklers {
-    import PrimitivePicklers._
-
-    override implicit def otherPickler: Pickler[(Long, Long)] = tuple2
     override def convertFrom(other: (Long, Long)): Interval = new Interval(other._1, other._2)
     override def convertTo(t: Interval): (Long, Long) = (t.getStartMillis, t.getEndMillis)
   }
